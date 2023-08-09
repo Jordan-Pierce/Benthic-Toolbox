92045de0bf0a7c57048e42747b9ed6912c59d18e
Jordan Pierce <jordan.pierce@noaa.gov>
2023-08-09 16:44:01 -0400

    coco

--- Toolbox/Tools/Detection/COCO.py
+++ Toolbox/Tools/Detection/COCO.py
@@ -5,40 +5,22 @@ import argparse
 import json
 import pandas as pd
 
+
 # ------------------------------------------------------------------------------------------------------------------
 # Functions
 # ------------------------------------------------------------------------------------------------------------------
 
-def coco(args):
+def concat_annotations(annotation_files):
     """
-    :param ann_file:
-    :param out_file:
+    :param annotation_files:
     :return:
     """
-    print("\n###############################################")
-    print("Converting to COCO")
-    print("###############################################\n")
 
-    if args.annotations:
-        annotation_files = args.annotations
-        annotations = pd.DataFrame()
-    else:
-        print(f"ERROR: No annotation files were provided as input")
-        sys.exit(1)
+    # Annotations
+    annotations = pd.DataFrame()
 
-    # Set the variables
-    output_dir = args.output_dir
-    os.makedirs(output_dir, exist_ok=True)
-
-    # Output coco annotation file
-    coco_file = f"{output_dir}\\coco_annotations.json"
-
-    # Output class map json file
-    class_map_file = f"{output_dir}\\class_map.json"
-
-    # To hold the coco formatted data
-    images = []
-    objects = []
+    if not annotation_files:
+        return annotations
 
     # Loop through the annotation files
     for annotation_file in annotation_files:
@@ -50,8 +32,27 @@ def coco(args):
             # Group all the annotations together into a single dataframe
             annotations = pd.concat((annotations, pd.read_csv(annotation_file, index_col=0)))
 
-    # Create a class mapping for category id (this means all annotation files must be added)
-    class_mapping = {v: i for i, v in enumerate(annotations['ScientificName'].unique())}
+    # Reset the index
+    annotations.reset_index(drop=True, inplace=True)
+
+    return annotations
+
+
+def to_coco(annotations, class_mapping, coco_file):
+    """
+    :param annotations:
+    :param class_map:
+    :param coco_file:
+    :return:
+    """
+
+    # If no annotations, return
+    if annotations.empty:
+        return
+
+    # To hold the coco formatted data
+    images = []
+    objects = []
 
     # Loop through first based on unique images
     for i_idx, image_name in enumerate(annotations['Image Name'].unique()):
@@ -82,7 +83,7 @@ def coco(args):
             data_anno = dict(
                 image_id=i_idx,
                 id=a_idx,
-                category_id=class_mapping[r['ScientificName']],
+                category_id=class_mapping[r['Scientific Name']],
                 bbox=[xmin, ymin, xmax, ymax],
                 segmentation=[mask_polygon],
                 area=(xmax - xmin) * (ymax - ymin),
@@ -108,6 +109,54 @@ def coco(args):
     else:
         print("ERROR: Could not save COCO formatted file")
 
+
+def coco(args):
+    """
+    :param ann_file:
+    :param out_file:
+    :return:
+    """
+    print("\n###############################################")
+    print("Converting to COCO")
+    print("###############################################\n")
+
+    # Set the variables
+    output_dir = args.output_dir
+    os.makedirs(output_dir, exist_ok=True)
+
+    # Output coco annotation files
+    train_file = f"{output_dir}\\train.json"
+    valid_file = f"{output_dir}\\valid.json"
+    test_file = f"{output_dir}\\test.json"
+
+    # Output class map json file
+    class_map_file = f"{output_dir}\\class_map.json"
+
+    # Get the annotations per dataset
+    train_annotations = concat_annotations(args.train_files)
+    valid_annotations = concat_annotations(args.valid_files)
+    test_annotations = concat_annotations(args.test_files)
+
+    # Single object detector
+    if args.single_object_detector:
+        train_annotations['Scientific Name'] = 'Object'
+        valid_annotations['Scientific Name'] = 'Object'
+        test_annotations['Scientific Name'] = 'Object'
+
+    # Combine
+    annotations = pd.concat((train_annotations, valid_annotations, test_annotations))
+
+    # Create a class mapping for category id (this means all annotation files must be added)
+    class_mapping = {v: i for i, v in enumerate(annotations['Scientific Name'].unique())}
+
+    # Create COCO format annotations for each of the datasets
+    to_coco(train_annotations, class_mapping, train_file)
+    to_coco(valid_annotations, class_mapping, valid_file)
+    to_coco(test_annotations, class_mapping, test_file)
+
+    # Updated class mapping file as expected by training script
+    categories = [{'id': class_id, 'name': class_name} for class_name, class_id in class_mapping.items()]
+
     # Write the JSON data to the output file
     with open(class_map_file, 'w') as output_file:
         json.dump(categories, output_file, indent=3)
@@ -129,8 +178,17 @@ def main():
 
     parser = argparse.ArgumentParser(description="COCO")
 
-    parser.add_argument("--annotations", type=str, nargs="+",
-                        help="Path to the annotation files")
+    parser.add_argument("--train_files", type=str, nargs="+",
+                        help="Path to the training annotation files")
+
+    parser.add_argument("--valid_files", type=str, nargs="+",
+                        help="Path to the validation annotation files")
+
+    parser.add_argument("--test_files", type=str, nargs="+",
+                        help="Path to the testing annotation files")
+
+    parser.add_argument("--single_object_detector", action='store_true',
+                        help="All labels are replaced with a single class category")
 
     parser.add_argument("--output_dir", type=str,
                         default=f"{os.path.abspath('../../../Data/Ground_Truth/')}",
